0f910417e1841f5e936f27ad8548de0418087779
VijayWhat <vijayrv042@gmail.com>
2025-01-02 16:16:16 +0530

    Initial commit for Flask app for render

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
+import os
 
-
-app = Flask(__name__, template_folder='templates') # Specify the template folder
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
-    
     name = "VJ"
-    return render_template('home.html' , name = name)
+    return render_template('home.html', name=name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render provides a PORT environment variable
+    app.run(debug=False, host='0.0.0.0', port=port)
