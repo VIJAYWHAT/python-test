89a3c32b8ba209d624a4a5c9417a60cd34051987
VijayWhat <vijayrv042@gmail.com>
2025-03-01 16:16:16 +0530

    Deploy Flask app with GitHub Actions

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-
 from flask import Flask, render_template
 
 
-app = Flask(__name__, static_folder='static') # Specify the template folder
+app = Flask(__name__, template_folder='templates') # Specify the template folder
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    
+    name = "VJ"
+    return render_template('home.html' , name = name)
 
 if __name__ == '__main__':
     app.run(debug=True)
