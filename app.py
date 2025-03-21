66434147f21f5ef02912e3bb8ff25349cf89ebb4
VijayWhat <vijayrv042@gmail.com>
2025-03-21 18:42:46 +0530

    port changes and static fol added

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
-import os
 
-app = Flask(__name__, template_folder='templates')
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def home():
+    
     name = "VJ"
-    return render_template('home.html', name=name)
+    return render_template('home.html' , name = name)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Render provides a PORT environment variable
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True)
