2b45ab9450a1954b1035020b851778da8ce96ca3
VijayWhat <vijayrv042@gmail.com>
2025-03-21 15:13:34 +0530

    Test files

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__, template_folder='templates', static_folder='static') # Specify the template folder
+app = Flask(__name__, static_folder='static') # Specify the template folder
 
 @app.route('/')
 def home():
